c7ef519c48e4f8c8b22c0257c2f1ef85221b17b5
Carlos Perez <carlos.perezprieto@sage.com>
2018-01-20 17:07:17 +0100

    Get/Set Led status

---
+++ server.rb
@@ -0,0 +1,55 @@
+# server.rb
+
+require 'sinatra'
+require 'sinatra/namespace'
+
+# Endpoints
+get '/' do
+  'Welcome to Raspian Server'
+end
+
+namespace '/api/v1' do
+  before do
+    content_type 'application/json'
+  end
+
+  get '/leds' do
+    # green = /sys/class/leds/led0/brightness
+    # red = /sys/class/leds/led1/brightness
+    leds = []
+
+    green = `cat /sys/class/leds/led1/brightness`.delete!("\n")
+    leds << Led.new('0', green)
+
+    red = `cat /sys/class/leds/led1/brightness`.delete!("\n")
+    leds << Led.new('0', red)
+    leds.to_json
+  end
+
+  put '/leds/:id/:status' do |id, status|
+    puts id + ' ' + status
+    halt(404, { message: 'Led Not Found' }.to_json) unless (0..1).include? id.to_i
+    `echo "#{status}" > /sys/class/leds/led"#{id}"/brightness`
+  end
+end
+
+# Models
+class Led
+  attr_accessor :name, :status
+
+  def initialize(name, status)
+    @name = name
+    @status = status
+  end
+
+  def as_json(options={})
+    {
+      name: @name,
+      status: @status
+    }
+  end
+
+  def to_json(*options)
+    as_json(*options).to_json(*options)
+  end
+end
