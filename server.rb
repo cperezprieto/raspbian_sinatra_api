21617b15d83b017140df47d7a49bec9043e81331
Carlos Perez <carlos.perezprieto@sage.com>
2018-03-28 06:22:42 +0000

    v2 for Home Assistant support

--- server.rb
+++ server.rb
@@ -30,6 +30,31 @@ namespace '/api/v1' do
     leds.to_json
   end
 
+  put '/leds/:id/:status' do |id, status|
+    puts id + ' ' + status
+    halt(404, { message: 'Led Not Found' }.to_json) unless (0..1).include? id.to_i
+    `sudo sh -c 'echo #{status} > /sys/class/leds/led#{id}/brightness'`
+  end
+end
+
+namespace '/api/v2' do
+  before do
+    content_type 'application/json'
+  end
+
+  get '/leds' do
+    # green = /sys/class/leds/led0/brightness
+    # red = /sys/class/leds/led1/brightness
+    leds = []
+
+    green = `cat /sys/class/leds/led0/brightness`.delete!("\n")
+    leds << Led.new('green', green, green.to_i.to_b?)
+
+    red = `cat /sys/class/leds/led1/brightness`.delete!("\n")
+    leds << Led.new('red', red, red.to_i.to_b?)
+    leds.to_json
+  end
+
   get '/leds/:led' do |led|
     case led.downcase
     when 'green'
@@ -42,10 +67,10 @@ namespace '/api/v1' do
     end
 
     status = `sudo sh -c "cat /sys/class/leds/led#{id}/brightness"`.delete!("\n")
-    Led.new(led, status).to_json
+    Led.new(led, status, status.to_i.to_b?).to_json
   end
 
-  put '/leds/:led/:status' do |led, status|
+  post '/leds/:led' do |led|
     case led.downcase
     when 'green'
       id = 0
@@ -55,27 +80,37 @@ namespace '/api/v1' do
       puts id + ' ' + status
       halt(404, { message: 'Led Not Found' }.to_json) unless (0..1).include? id.to_i
     end
-    `sudo sh -c "echo #{status} > /sys/class/leds/led#{id}/brightness"`
+
+    status = JSON.parse(request.body.read)
+    `sudo sh -c "echo #{status.values[0]} > /sys/class/leds/led#{id}/brightness"`
   end
 end
 
 # Models
 class Led
-  attr_accessor :name, :status
+  attr_accessor :name, :status, :is_active
 
-  def initialize(name, status)
+  def initialize(name, status, is_active = nil)
     @name = name
     @status = status
+    @is_active = is_active
   end
 
   def as_json(options={})
     {
       name: @name,
-      status: @status
+      status: @status,
+      is_active: @is_active
     }
   end
 
   def to_json(*options)
-    as_json(*options).to_json(*options)
+    as_json(*options).reject { |k, v| v.nil? }.to_json(*options)
+  end
+end
+
+class Integer
+  def to_b?
+    !self.zero?
   end
 end
